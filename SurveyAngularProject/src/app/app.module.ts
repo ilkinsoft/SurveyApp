717ed9f497c73522fb0cf3c66094239088ca20ad
Hisham Mounir <hisham.mounir@hotmail.com>
2019-06-15 21:54:22 -0500

    commit template and login page

--- SurveyAngularProject/src/app/app.module.ts
+++ SurveyAngularProject/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './layout/app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './routing';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
