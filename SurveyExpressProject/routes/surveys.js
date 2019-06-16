84336499abbc49f59d468f1d69c2e6bdf619d28d
İlkin Elimov <e-ilkin@msn.com>
2019-06-15 23:58:25 -0500

    POST request and sigleton pattern is added into surveys route.

--- SurveyExpressProject/routes/surveys.js
+++ SurveyExpressProject/routes/surveys.js
@@ -1,8 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://admin:surveysystem1234@cluster0-4k3cn.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true })
+let db
+let collection
+
+router.use((req, res, next) => {
+    if (!db) {
+        client.connect(function (err) {
+            db = client.db('mwa')
+            req.db = db
+            collection = db.collection('surveys')
+            next()
+        })
+    }
+    else {
+        req.db = db
+        collection = db.collection('surveys')
+        next()
+    }
+})
+
 
 router.get('/', function (req, res, next) {
     res.send('surveyy');
 });
+
+router.post('/', async function (req, res, next) {
+
+    if (!req.body) {
+        // 400 Bad Request
+        res.status(400).send('This is a bad request, make sure all fields are correct.')
+        return;
+    }
+
+    // res.send(survey);
+
+    await collection.insertOne(req.body, function (err, result) {
+        if (err)
+            console.log("Error: " + err);
+
+        console.log('added survey')
+        res.json(result);
+    })
+});
+
 
 module.exports = router;
