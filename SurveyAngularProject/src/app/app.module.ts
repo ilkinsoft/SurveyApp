944743464d810857e73eb2ed67930f3d17cdcdfb
Andrew Emad <andrew@maxycode.com>
2019-06-16 01:15:25 -0500

    add the design

--- SurveyAngularProject/src/app/app.module.ts
+++ SurveyAngularProject/src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './layout/app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from './routing';
+import { HomeComponent } from './home/home.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {DemoMaterialModule} from './material-module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, LoginComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    DemoMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
