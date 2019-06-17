58d14e35054415eb72aa4c9271a8943c601804f5
zmeid <zmeid.krtz@gmail.com>
2019-06-17 15:15:06 -0500

    -Merging issues fix started

--- SurveyAngularProject/src/app/app.module.ts
+++ SurveyAngularProject/src/app/app.module.ts
@@ -5,14 +5,10 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './routing';
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule, /* other http imports */ } from "@angular/common/http";
 
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {DemoMaterialModule} from './material-module';
 import { CreateSurveyComponent } from './create-survey/create-survey.component';
 import {ToastrModule} from "ngx-toastr";
 import {CommonModule} from "@angular/common";
@@ -21,7 +17,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { DemoMaterialModule } from './material-module';
 import { SurveyComponent } from './survey/survey.component';
-import { HttpClientModule } from '@angular/common/http';
 import { SurvayDetailsComponent } from './survay-details/survay-details.component';
 import { ChartsModule } from 'ng2-charts';
 import { MulitchoiseQuestionComponent } from './mulitchoise-question/mulitchoise-question.component';
@@ -29,7 +24,7 @@ import { ArticleQuestionComponent } from './article-question/article-question.co
 import { MomentModule } from 'angular2-moment';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, LoginComponent, HomeComponent, SurveyComponent, SurvayDetailsComponent, MulitchoiseQuestionComponent, ArticleQuestionComponent],
+  declarations: [RegisterComponent,CreateSurveyComponent,AppComponent, HeaderComponent, FooterComponent, LoginComponent, HomeComponent, SurveyComponent, SurvayDetailsComponent, MulitchoiseQuestionComponent, ArticleQuestionComponent],
   imports: [
     BrowserModule,
     CommonModule,
