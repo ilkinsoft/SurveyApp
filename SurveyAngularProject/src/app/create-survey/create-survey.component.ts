68d16bb8a2352623eb912c847d9929ca917385dc
İlkin Elimov <e-ilkin@msn.com>
2019-06-17 15:13:38 -0500

    Open-ended questions are added.

--- SurveyAngularProject/src/app/create-survey/create-survey.component.ts
+++ SurveyAngularProject/src/app/create-survey/create-survey.component.ts
@@ -18,7 +18,7 @@ export class CreateSurveyComponent implements OnInit {
   choices: [];
   choiceList: any;
 
-  resultSurvey={title:"",createdBy:"",createdAt:"",questions:[]};
+  resultSurvey = { title: "", createdBy: "", createdAt: "", questions: [] };
 
   constructor(private formBuilder: FormBuilder, private myHttpService: MyHttpServiceService, private toastr: ToastrService) {
 
@@ -28,13 +28,9 @@ export class CreateSurveyComponent implements OnInit {
       'createdBy': ['', Validators.compose([Validators.required, Validators.email])],
       'createdAt': ['', Validators.compose([Validators.required])],
       'questions': this.formBuilder.array([this.createQuestion()]),
-
-      // 'birthday': ['', Validators.compose([Validators.required])],
-      // 'maritalStatus': ['', Validators.compose([Validators.required])],
-      // 'country': ['', Validators.compose([Validators.required])],
     });
 
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < 9; i++) {
       this.addQuestion()
     }
     // for (let k = 0; k < 3; k++)
@@ -56,57 +52,60 @@ export class CreateSurveyComponent implements OnInit {
     });
   }
 
-  createChoice(): FormGroup {
-    // return this.formBuilder.array()
-    // this.addChoice()
+  // createChoice(): FormGroup {
+  //   // return this.formBuilder.array()
+  //   // this.addChoice()
 
-    return this.formBuilder.group({      
-      0: ['', Validators.compose([Validators.required])],
-      1: ['', Validators.compose([Validators.required])],
-      2: ['', Validators.compose([Validators.required])],
-      3: ['', Validators.compose([Validators.required])]
-    })
-  }
+  //   return this.formBuilder.group({      
+  //     0: ['', Validators.compose([Validators.required])],
+  //     1: ['', Validators.compose([Validators.required])],
+  //     2: ['', Validators.compose([Validators.required])],
+  //     3: ['', Validators.compose([Validators.required])]
+  //   })
+  // }
 
-  addChoice(): void {
-    this.choiceList = this.surveyForm.get('choices') as FormArray;
+  // addChoice(): void {
+  //   this.choiceList = this.surveyForm.get('choices') as FormArray;
 
-    // this.choiceList = this.questionList.controls[0].controls.choices;
-    // console.log(this.choiceList)
-    // for (let i = 0; i < this.questionList.length; i++) {
-    //   this.questionList[i].push(this.createChoice())
-    // }
-    this.choiceList.push(this.createChoice());
-  }
+  //   // this.choiceList = this.questionList.controls[0].controls.choices;
+  //   // console.log(this.choiceList)
+  //   // for (let i = 0; i < this.questionList.length; i++) {
+  //   //   this.questionList[i].push(this.createChoice())
+  //   // }
+  //   this.choiceList.push(this.createChoice());
+  // }
 
   ngOnInit() {
   }
 
   parseJson() {
-    this.resultSurvey.title=this.surveyForm.value.title;
-    this.resultSurvey.createdBy="somebody";
-    this.resultSurvey.createdAt= new Date().toString();
-    this.resultSurvey.questions=[];
+    this.resultSurvey.title = this.surveyForm.value.title;
+    this.resultSurvey.createdBy = "somebody";
+    this.resultSurvey.createdAt = new Date().toString();
+    this.resultSurvey.questions = [];
 
     //console.log(this.surveyForm.value.questions);
 
-    for(let i=0;i<this.surveyForm.value.questions.length;i++){
+    for (let i = 0; i < this.surveyForm.value.questions.length; i++) {
       let tempQuestion = this.surveyForm.value.questions[i];
-      //console.log("boommmmtemp"+tempQuestion);
       let textOfQuestion = tempQuestion.question;
-      let choices=[];
-      choices.push(tempQuestion.choice1);
-      choices.push(tempQuestion.choice2);
-      choices.push(tempQuestion.choice3);
-      choices.push(tempQuestion.choice4);
+      let choices = [];
 
-      let questionObject={question:{},choices:[]};
-      questionObject.question=textOfQuestion;
-      questionObject.choices=choices;
+      if (i < 7) // for multi-choice questions add choices
+      {
+        choices.push(tempQuestion.choice1);
+        choices.push(tempQuestion.choice2);
+        choices.push(tempQuestion.choice3);
+        choices.push(tempQuestion.choice4);
+      }
+
+      let questionObject = { question: {}, choices: [] };
+      questionObject.question = textOfQuestion;
+
+      questionObject.choices = choices;
 
       this.resultSurvey.questions.push(questionObject);
     }
-    // console.log(this.surveyForm.value);
     console.log(this.resultSurvey);
   }
 
@@ -114,16 +113,15 @@ export class CreateSurveyComponent implements OnInit {
 
     this.parseJson()
     let body = JSON.parse(JSON.stringify(this.resultSurvey))
-    console.log(body)
 
-    this.myHttpService.post('surveys/add', body).subscribe((result: any) =>{
+    this.myHttpService.post('surveys/add', body).subscribe((result: any) => {
       //console.log("success")
 
       // console.log(this.resultSurvey)
-      if(result.code === 'SUCCESS'){
-        this.toastr.success('Yaay, Created successfully!', 'Success!',{timeOut:2000, positionClass: 'toast-top-center'});
-      }else{
-        this.toastr.error("Something went wrong!", 'Error :(',{timeOut:2000, positionClass: 'toast-top-center'});
+      if (result.code === 'SUCCESS') {
+        this.toastr.success('Yaay, Created successfully!', 'Success!', { timeOut: 2000, positionClass: 'toast-top-center' });
+      } else {
+        this.toastr.error("Something went wrong!", 'Error :(', { timeOut: 2000, positionClass: 'toast-top-center' });
       }
       console.log(result);
     });
