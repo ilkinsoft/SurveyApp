edf51ba68dfaeadb0895cb49bc676412d49b274c
zmeid <zmeid.krtz@gmail.com>
2019-06-17 15:18:01 -0500

    -Merging issues fix started

--- SurveyAngularProject/src/app/survey/survey.component.ts
+++ SurveyAngularProject/src/app/survey/survey.component.ts
@@ -36,7 +36,7 @@ export class SurveyComponent {
   }
 
   set_products() {
-    this.httpClient.get('http://localhost:3000/survay').subscribe((res) => {
+    this.httpClient.get('http://localhost:3000/surveys').subscribe((res) => {
       console.log(JSON.stringify(res));
       this.ELEMENT_DATA = JSON.parse(JSON.stringify(res));
       this.dataSource = new MatTableDataSource<Element>(this.ELEMENT_DATA);
